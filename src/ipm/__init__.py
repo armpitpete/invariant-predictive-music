b8d245f836ec71427364c3089c244dbbaca1a838
Merrin W. Dream <merrin@merrinworld.uk>
2026-08-17 14:28:35 +0100

    Export sonority scoring API

--- src/ipm/__init__.py
+++ src/ipm/__init__.py
@@ -2,11 +2,37 @@
 
 from .model import IPMConfig, NoteEvent, Voice, VoiceOverlapError
 from .randomness import SeededRandom
+from .sonority import (
+    ActiveNote,
+    SonoritySlice,
+    TextureScore,
+    VerticalScore,
+    contextual_pair_score,
+    interval_class,
+    interval_prior,
+    metrical_strength,
+    score_sonority,
+    score_texture,
+    set_coherence,
+    slice_active_sonorities,
+)
 
 __all__ = [
+    "ActiveNote",
     "IPMConfig",
     "NoteEvent",
     "SeededRandom",
+    "SonoritySlice",
+    "TextureScore",
+    "VerticalScore",
     "Voice",
     "VoiceOverlapError",
+    "contextual_pair_score",
+    "interval_class",
+    "interval_prior",
+    "metrical_strength",
+    "score_sonority",
+    "score_texture",
+    "set_coherence",
+    "slice_active_sonorities",
 ]
